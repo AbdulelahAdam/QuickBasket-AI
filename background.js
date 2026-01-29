acd3cde1967a8ea591225a7777268eeb67b07644
Abdulelah Adam <abdulelah859@gmail.com>
2026-01-29 04:41:21 +0200

    minor fixes

--- background.js
+++
@@ -1,9 +0,0 @@
-chrome.runtime.onMessage.addListener((msg, sender) => {
-  if (msg.type === "TRACK_PRODUCT") {
-    fetch("http://localhost:8000/track/browser", {
-      method: "POST",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify(msg.payload),
-    });
-  }
-});
